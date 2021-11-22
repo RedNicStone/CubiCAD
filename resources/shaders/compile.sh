3ce5181049773666826a4024d2a5767efedcf010
RedNicStone <RedNicStone@users.noreply.github.com>
2021-11-22 12:43:04 +0000

    deploy: 85055d5bc5d80d2bc5e40875892179487d57c198

--- resources/shaders/compile.sh
+++
@@ -1,4 +0,0 @@
-VULKAN_SDK_PATH=/home/nic/vulkan/vulkansdk-linux-x86_64-1.2.182.0/1.2.182.0/x86_64
-
-$VULKAN_SDK_PATH/bin/glslc quad.vert -o vert_quad.spv
-$VULKAN_SDK_PATH/bin/glslc uv.frag -o frag_uv.spv
--- src/renderer/vulkan/pipelinebase.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by nic on 18/07/2021.
-//
-
-#include "pipelinebase.h"
