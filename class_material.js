6d2ad3e7e0bcc75e6b7a15f7f61d17439f0361d7
RedNicStone <RedNicStone@users.noreply.github.com>
2022-01-11 11:04:00 +0000

    deploy: b6a66d579b9e1a47a221c5b0db469819d199564c

--- class_material.js
+++ class_material.js
@@ -1,6 +1,7 @@
 var class_material =
 [
     [ "getMasterMaterial", "class_material.html#a7d00fcace7b41720b2c7fba99e87f3fd", null ],
+    [ "getMasterMaterialBindings", "class_material.html#a73e6a263dfef31935a897c0708b23d8e", null ],
     [ "getParameters", "class_material.html#a27dee92d49ed6b526ebec2d5afb3e19e", null ],
     [ "masterMaterial", "class_material.html#a44f31ce43a9f4a9cc7716162bc652b34", null ],
     [ "parameters", "class_material.html#aad7e884ad1cb3f77a55b692ce751d53b", null ]
