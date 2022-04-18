c2cca6f9e2f09a0a44cf5075cb920d70d03f7413
RedNicStone <RedNicStone@users.noreply.github.com>
2022-04-18 22:19:25 +0000

    deploy: 7cfaf8bd02c76f8ab73a0e927d4eba6ea4f1fdd9

--- struct_meshlet.js
+++ struct_meshlet.js
@@ -1,5 +1,6 @@
 var struct_meshlet =
 [
+    [ "firstIndex", "struct_meshlet.html#a2cb0f50c67585acbfd77649f9b9cbd38", null ],
     [ "indexData", "struct_meshlet.html#ac2c0e3008a1b24bf31698e327d6ebc53", null ],
     [ "material", "struct_meshlet.html#aa573cdbdbc8d9c27391c1f79a9390952", null ]
 ];
